9fb04836c04cced89e0b3e48824bfbd101849286
juro0 <78843415+Juro0@users.noreply.github.com>
2024-12-01 15:30:56 +0100

    Added template and gitignore
    Added template.py, containing a template for the code for the daily puzzles.
    Added gitignore, preventing the push of input.txt files, according to the Terms of Service of AoC.

---
+++ template.py
@@ -0,0 +1,29 @@
+from _ import _
+
+def format_input(year, day):
+
+    with open(f'{year}/inputs/day-{day}.txt') as f:
+        lines = f.readlines()
+    
+    # code
+    
+    return lines
+
+def first_part(i):
+    
+    # code
+    
+    return _
+
+def second_part(i):
+    
+    # code
+    
+    return _
+
+if __name__ == '__main__':
+    
+    i = format_input(_, _)
+    
+    print('First part solution: ' + first_part(i))
+    print('Second part solution: ' + second_part(i))
